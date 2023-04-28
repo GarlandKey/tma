abae3b22eb8c3b62c491f1215d1c8fb156e6f18a
Garland Key <hello@garlandkey.com>
2023-04-28 13:23:58 -0400

    added routes

---
+++ server/src/routes/tasks-routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const taskController = require('../controllers/tasks')
+
+router.get('/tasks', taskController.getTasks)
+
+router.post('/tasks', taskController.createTask)
+
+router.put('/tasks/:id', taskController.updateTask)
+
+router.delete('/delete', taskController.deleteTask)
+
+module.exports = router
