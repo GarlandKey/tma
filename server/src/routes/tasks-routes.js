452f9f7e55e832ab04ce9f0587e80d35cc4e22a9
Garland Key <hello@garlandkey.com>
2023-04-28 13:26:22 -0400

    fixed an import path error in routes

--- server/src/routes/tasks-routes.js
+++ server/src/routes/tasks-routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const taskController = require('../controllers/tasks')
+const taskController = require('../controllers/tasks-controller')
 
 router.get('/tasks', taskController.getTasks)
 
